88eb18ed0d6f2c78faede1ca84abe55d37bfedfc
kreothesuper <108620658+kreothesuper@users.noreply.github.com>
2022-09-14 18:51:23 +0300

    Add files via upload

--- first/js/common.js
+++ first/js/common.js
@@ -241,12 +241,11 @@ const apiRequest = (keywordSearch) => {
     const api_url = 'https://wbads.topseller.ru/advert/wb_info_by_keyword/',
         api_options = {
             method: "POST",
+            mode: 'cors',
             referrerPolicy: 'no-referrer',
             body: JSON.stringify({keyword: keywordSearch}),
             headers: {
                 "Content-type": "application/json; charset=UTF-8",
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'POST',
             }
         }
 
