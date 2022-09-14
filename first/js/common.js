ac7bb4890b59babf4aaadea1c657173b0c5c8c1f
kreothesuper <108620658+kreothesuper@users.noreply.github.com>
2022-09-14 17:12:30 +0300

    Add files via upload

--- first/js/common.js
+++ first/js/common.js
@@ -245,7 +245,8 @@ const apiRequest = (keywordSearch) => {
             body: JSON.stringify({keyword: keywordSearch}),
             headers: {
                 "Content-type": "application/json; charset=UTF-8",
-                'Access-Control-Allow-Origin': '*'
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'POST',
             }
         }
 
