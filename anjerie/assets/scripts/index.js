713303ddc81963b80303644bc18cf635b1041789
kreotib <kreotib@gmail.com>
2023-12-08 09:27:54 +0300

    1234

--- anjerie/assets/scripts/index.js
+++ anjerie/assets/scripts/index.js
@@ -1,17 +1,72 @@
-document.addEventListener('DOMContentLoaded',()=>{
-    const burger = document.querySelector('.burger');
+const checkTargetOrKeyNav = event => {
+    console.log(event.target);
+    if (
+        event.target.classList.contains('nav__sublist') || 
+        event.key === 'Escape'
+    ) {
+        hideAllNavItems();
+    }
+};
+const showNavItem = navItem => {
+    const header = document.querySelector('.header');
 
-    burger.addEventListener('click',(e)=>{
+    if (navItem.classList.contains('nav__item--active')) return;
+
+    hideAllNavItems();
+
+    document.addEventListener('click', checkTargetOrKeyNav);
+    document.addEventListener('keyup', checkTargetOrKeyNav);
+
+    header.classList.add('header--open');
+    navItem.classList.add('nav__item--active');
+};
+
+const hideAllNavItems = () => {
+    const header = document.querySelector('.header'),
+        navItemArray = document.querySelectorAll('.nav__item');
+
+    header.classList.remove('header--open');
+
+    navItemArray.forEach(element => {
+        element.classList.remove('nav__item--active')
+    });
+
+    document.removeEventListener('click', checkTargetOrKeyNav);
+    document.removeEventListener('keyup', checkTargetOrKeyNav);
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.querySelector('.burger');
+    const header = document.querySelector('.header');
+
+    burger.addEventListener('click', (e) => {
         e.preventDefault();
 
+        header.classList.toggle('header--open');
         burger.classList.toggle('burger--active');
     });
 
 
-    const gallerySlider = new Swiper('.js-gallery-slider',{
-        slidesPerView:'auto',
-        spaceBetween:22,
-        loop:true,
-        speed:500,
+    const gallerySlider = new Swiper('.js-gallery-slider', {
+        slidesPerView: 'auto',
+        spaceBetween: 22,
+        loop: true,
+        speed: 500,
     });
+
+
+    const sublistArray = document.querySelectorAll('.nav__sublist');
+
+    if (sublistArray.length) {
+        sublistArray.forEach(sublist => {
+            const navItem = sublist.closest('.nav__item'),
+                navLink = navItem.querySelector('.nav__link');
+
+            navLink.addEventListener('click', (e) => {
+                e.preventDefault();
+
+                showNavItem(navItem);
+            });
+        });
+    }
 });
