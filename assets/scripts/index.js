12d3d8711a4f569acfeb289eaaca3e7a4f2e4ec1
kreotib <kreotib@gmail.com>
2023-12-14 06:39:41 +0300

    214

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,40 +1,40 @@
-document.addEventListener('DOMContentLoaded',()=>{
+document.addEventListener('DOMContentLoaded', () => {
 
     const sliderScrollbar = {
-        el:'.slider-scrollbar',
-        dragClass:'slider-scrollbar__drag'
+        el: '.slider-scrollbar',
+        dragClass: 'slider-scrollbar__drag'
     }
-    const partnersSlider = new Swiper('.js-partners-slider',{
-        slidesPerView:'auto',
-        spaceBetween:50,
-        breakpoints:{
-            769:{
-                spaceBetween:86,
+    const partnersSlider = new Swiper('.js-partners-slider', {
+        slidesPerView: 'auto',
+        spaceBetween: 50,
+        breakpoints: {
+            769: {
+                spaceBetween: 86,
             }
         }
     });
-    const roadmapSlider = new Swiper('.js-roadmap-slider',{
-        slidesPerView:'auto',
-        spaceBetween:20,
-        scrollbar:sliderScrollbar,
+    const roadmapSlider = new Swiper('.js-roadmap-slider', {
+        slidesPerView: 'auto',
+        spaceBetween: 20,
+        scrollbar: sliderScrollbar,
     });
-    const teamSlider = new Swiper('.js-team-slider',{
-        slidesPerView:'auto',
-        spaceBetween:20,
-        scrollbar:sliderScrollbar,
+    const teamSlider = new Swiper('.js-team-slider', {
+        slidesPerView: 'auto',
+        spaceBetween: 20,
+        scrollbar: sliderScrollbar,
     });
 
 
     const faq = document.querySelector('.faq');
 
-    if(faq){
+    if (faq) {
         const faqItemArray = faq.querySelectorAll('.faq-item');
 
-        if(faqItemArray.length){
-            faqItemArray.forEach(faqItem=>{
+        if (faqItemArray.length) {
+            faqItemArray.forEach(faqItem => {
                 const faqItemHeader = faqItem.querySelector('.faq-item__header');
 
-                faqItemHeader.addEventListener("click",(e)=>{
+                faqItemHeader.addEventListener("click", (e) => {
                     e.preventDefault();
 
                     faqItem.classList.toggle('faq-item--active');
@@ -46,19 +46,19 @@ document.addEventListener('DOMContentLoaded',()=>{
     const burger = document.querySelector('.burger');
     const headerNav = document.querySelector('.header__nav');
 
-    if(burger && headerNav){
+    if (burger && headerNav) {
         const navLinkArray = document.querySelectorAll('.nav__link');
 
-        if(navLinkArray.length){
-            navLinkArray.forEach(navLink=>{
-                navLink.addEventListener('click',()=>{
+        if (navLinkArray.length) {
+            navLinkArray.forEach(navLink => {
+                navLink.addEventListener('click', () => {
                     burger.classList.remove('burger--active');
-            headerNav.classList.remove('header__nav--active');    
+                    headerNav.classList.remove('header__nav--active');
                 })
             })
         }
 
-        burger.addEventListener('click',(e)=>{
+        burger.addEventListener('click', (e) => {
             e.preventDefault();
 
             burger.classList.toggle('burger--active');
@@ -67,5 +67,41 @@ document.addEventListener('DOMContentLoaded',()=>{
     }
 
 
-  
+    const timer = document.querySelector('.timer');
+
+    if (timer) {
+        const timerDay = timer.querySelector('.js-timer-day'),
+            timerHours = timer.querySelector('.js-timer-hours'),
+            timerMins = timer.querySelector('.js-timer-mins'),
+            timerSec = timer.querySelector('.js-timer-sec'),
+            timerLine = timer.querySelector('.js-timer-progress');
+
+
+        const countdownDate = new Date("December 31, 2023 23:59:59").getTime();
+
+        const countdownInterval = setInterval(function () {
+            const now = new Date().getTime();
+
+            const distance = countdownDate - now;
+
+            const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            timerDay.innerHTML = days;
+            timerHours.innerHTML = hours;
+            timerMins.innerHTML = minutes;
+            timerSec.innerHTML = seconds;
+
+            if (distance < 0) {
+                clearInterval(countdownInterval);
+            }
+        }, 1000);
+    }
+
+
+
+
+
 });
