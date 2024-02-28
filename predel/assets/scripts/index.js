0249db35a1a85d4dfec9c5e6354d6fdb7a253b42
kreotib <kreotib@gmail.com>
2024-02-28 14:47:07 +0300

    1245

--- predel/assets/scripts/index.js
+++ predel/assets/scripts/index.js
@@ -30,8 +30,101 @@ const initTabs = () => {
 }
 
 
+const checkTargetOrKey = event => {
+    if (
+        event.target.classList.contains('popup__wrapper') ||
+        event.key === 'Escape' ||
+        event.target.closest('.popup-close')
+    ) {
+        event.preventDefault();
+        hideAllPopups();
+    }
+};
+
+
+
+class Animations {
+    constructor() {
+        this.animationWrapperArray = document.querySelectorAll('.animation-wrapper');
+    }
+
+    toggleAnimation(animationWrapperElement) {
+        const animationItemArray = animationWrapperElement.querySelectorAll('.animation-item');
+        animationItemArray.forEach((animationItemElement, animationItemIndex) => {
+            animationItemElement.style.animationDelay = `${animationItemIndex * 0.2}s`;
+            animationItemElement.classList.toggle('animation-item--animated');
+        });
+    }
+
+    handleIntersection(entries) {
+        entries.forEach(entry => {
+            const animationItemArray = entry.target.querySelectorAll('.animation-item');
+            if (entry.isIntersecting) {
+                animationItemArray.forEach((animationItemElement, animationItemIndex) => {
+                    animationItemElement.classList.add('visible');
+                });
+            }else{
+                animationItemArray.forEach((animationItemElement, animationItemIndex) => {
+                    animationItemElement.classList.remove('visible');
+                });
+            }
+        });
+    }
+
+    init() {
+        if (!this.animationWrapperArray.length) return;
+
+        const observer = new IntersectionObserver(this.handleIntersection.bind(this),{
+        });
+        this.animationWrapperArray.forEach(animationWrapper => {
+            observer.observe(animationWrapper);
+        });
+    }
+}
+
+const centerElementVertically = (element) => {
+    const parentHeight = element.parentNode.clientHeight;
+    const elementHeight = element.querySelector('.popup__container').clientHeight;
+
+    console.log(element.querySelector('.popup__container').getBoundingClientRect().height,)
+    const topMargin = (parentHeight - elementHeight) / 2;
+    return `${0}px`
+}
+
+const showPopup = popupId => {
+    const popup = document.querySelector(popupId);
+    if (!popup) return
+
+    hideAllPopups();
+
+    popup.style.setProperty('--popup-margin',`${centerElementVertically(popup)}`)
+
+    popup.classList.add('popup--active');
+    document.body.classList.add('no-scroll');
+
+    document.addEventListener('click', checkTargetOrKey);
+    document.addEventListener('keyup', checkTargetOrKey);
+};
+
+const hideAllPopups = () => {
+    const popups = document.querySelectorAll('.popup');
+
+    popups.forEach(popup => {
+        popup.classList.remove('popup--active');
+    });
+    document.body.classList.remove('no-scroll');
+
+    document.removeEventListener('click', checkTargetOrKey);
+    document.removeEventListener('keyup', checkTargetOrKey);
+};
+
+
 document.addEventListener('DOMContentLoaded',()=>{
     initTabs();
+
+    //animation init
+    const animation = new Animations();
+    animation.init();
 
     const burger = document.querySelector('.burger'),
         headerNav = document.querySelector('.header__nav');
@@ -49,6 +142,12 @@ document.addEventListener('DOMContentLoaded',()=>{
         slidesPerView:'auto',
         spaceBetween:15,
         loop:true,
+        speed:1000,
+        autoplay:{
+            delay:3000,
+        },
+        loopAdditionalSlides:0,
+        loopPreventsSliding:false,
         breakpoints:{
             640:{
                 spaceBetween:20,
@@ -73,6 +172,55 @@ document.addEventListener('DOMContentLoaded',()=>{
             window.addEventListener('resize',()=>{
                 setScanStyle(scanBlock,scanItem);
             })
+        });
+    }
+
+    const jsFileArray = document.querySelectorAll('.js-file');
+
+    if (jsFileArray.length) {
+        jsFileArray.forEach(file => {
+            const fileInput = file.querySelector('.js-file-input'),
+                fileImg = file.querySelector('.js-file-img'),
+                fileName = file.querySelector('.js-file-name');
+
+
+            fileInput.addEventListener('change', () => {
+                const file = fileInput.files[0];
+                const reader = new FileReader();
+                reader.onload = (event) => {
+                    fileImg.src = event.target.result
+                }
+                reader.readAsDataURL(file);
+                fileName.innerHTML = file.name;
+            })
+        });
+    }
+
+    const formArray = document.querySelectorAll('.form');
+
+    if(formArray.length){
+        formArray.forEach(form=>{
+            form.addEventListener('submit',(e)=>{
+                e.preventDefault();
+
+                showPopup('.popup-thanks')
+            });
+        });
+    }
+
+
+    // popup init
+    const popupButtons = document.querySelectorAll('[data-popup]');
+    const popups = document.querySelectorAll('.popup');
+
+    if (popups.length && popupButtons.length) {
+        popupButtons.forEach(button => {
+            button.addEventListener('click', (e) => {
+                e.preventDefault();
+
+                const popupId = button.dataset.popup
+                showPopup(popupId);
+            });
         });
     }
 });
