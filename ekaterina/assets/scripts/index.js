521154b8ab18ced2a654bede7957b30847c94d16
kreotib <kreotib@gmail.com>
2024-04-08 20:27:50 +0300

    1234

--- ekaterina/assets/scripts/index.js
+++ ekaterina/assets/scripts/index.js
@@ -582,6 +582,15 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     }
+
+    (function init100vh(){
+        function setHeight() {
+            var vh = window.innerHeight * 0.01;
+            document.documentElement.style.setProperty('--vh', `${vh}px`);
+        }
+        setHeight();
+        window.addEventListener('resize', setHeight);
+    })();
 });
 
 window.addEventListener('load',()=>{
