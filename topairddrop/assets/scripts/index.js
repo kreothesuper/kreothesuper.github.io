286baad3306377df094b9abac0fe52b7219cac82
kreotib <kreotib@gmail.com>
2024-06-11 21:19:51 +0300

    1234

--- topairddrop/assets/scripts/index.js
+++ topairddrop/assets/scripts/index.js
@@ -1,3 +1,40 @@
+const checkTargetOrKey = event => {
+    if (
+        event.target.classList.contains('popup__wrapper') ||
+        event.key === 'Escape' ||
+        event.target.closest('.popup-close')
+    ) {
+        hideAllPopups();
+    }
+};
+const showPopup = popupId => {
+    const popup = document.querySelector(popupId);
+    if (!popup) return
+
+    const pageWrapper = document.querySelector('body');
+
+    hideAllPopups();
+
+    popup.classList.add('popup--active');
+    pageWrapper.classList.add('no-scroll');
+
+    document.addEventListener('click', checkTargetOrKey);
+    document.addEventListener('keyup', checkTargetOrKey);
+};
+
+const hideAllPopups = () => {
+    const popups = document.querySelectorAll('.popup'),
+        pageWrapper = document.querySelector('body');
+
+    popups.forEach(popup => {
+        popup.classList.remove('popup--active');
+    });
+    pageWrapper.classList.remove('no-scroll');
+
+    document.removeEventListener('click', checkTargetOrKey);
+    document.removeEventListener('keyup', checkTargetOrKey);
+};
+
 document.addEventListener('DOMContentLoaded',()=>{
     const burger = document.querySelector('.burger');
     const header = document.querySelector('.header')
@@ -8,4 +45,31 @@ document.addEventListener('DOMContentLoaded',()=>{
         header.classList.toggle('header--open')
         burger.classList.toggle('burger--active');
     });
+
+    const projectSlider = new Swiper('.project-slider', {
+        spaceBetween:20,
+        slidesPerView:'auto',
+      
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.project-slider__button--next',
+          prevEl: '.project-slider__button--prev',
+        },
+      });
+
+      const popupButtons = document.querySelectorAll('[data-popup]');
+      const popups = document.querySelectorAll('.popup');
+  
+      if (popups.length) {
+          popupButtons.forEach(button => {
+              console.log(popupButtons)
+              button.addEventListener('click', (e) => {
+                  e.preventDefault();
+  
+                  const popupId = button.dataset.popup
+                  showPopup(popupId);
+              });
+          });
+      }
 });
