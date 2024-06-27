ad9ab290552572621e9fd42115e9e9e1c1e03844
kreotib <kreotib@gmail.com>
2024-06-27 14:40:49 +0300

    124

--- ghs/assets/scripts/index.js
+++ ghs/assets/scripts/index.js
@@ -157,14 +157,21 @@ document.addEventListener('DOMContentLoaded', () => {
         videoArray.forEach(video => {
             const videoElement = document.querySelector('video');
          
-            video.addEventListener('click', function () {
-                if (videoElement.paused) {
-                    videoElement.play();
-                    video.classList.add('play');
-                } else {
-                    videoElement.pause();
-                    video.classList.remove('play');
+            video.addEventListener('click', function (e) {
+                console.log(e.target.classList.contains('video__resize'));
+                if(!e.target.classList.contains('video__resize')){
+                    if (videoElement.paused) {
+                        videoElement.play();
+                        video.classList.add('play');
+                    } else {
+                        videoElement.pause();
+                        video.classList.remove('play');
+                        video.classList.remove('full');
+                    }
+                }else{
+                    video.classList.toggle('full');
                 }
+                
             });
         })
     }
