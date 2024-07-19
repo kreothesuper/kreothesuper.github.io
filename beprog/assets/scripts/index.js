2b4b2b13ff626a142e4e34e90adaf786772fc8a1
kreotib <kreotib@gmail.com>
2024-07-19 14:15:51 +0300

    1234

--- beprog/assets/scripts/index.js
+++ beprog/assets/scripts/index.js
@@ -23,6 +23,41 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     }
 
+    const hero = document.querySelector('.hero');
+
+    var swiper = new Swiper('.hero', {
+        spaceBetween: 30,
+        effect: 'fade',
+        speed:1500,
+        mousewheel: {
+            invert: false,
+            // forceToAxis: true,
+            sensitivity: 1,
+            releaseOnEdges: true,
+            thresholdTime:500,
+        },
+     
+        autoHeight: true,
+    });
+
+    swiper.on('slideChange', function () {
+        hero.dataset.current = swiper.realIndex;
+        hero.scrollIntoView();
+    });
+
+    const heroSection = document.querySelectorAll('.hero-section');
+
+    heroSection.forEach(element=>{
+        const heroSectionText = element.querySelectorAll('.hero-section-text');
+
+        if(heroSectionText.length){
+            heroSectionText.forEach((text,index) =>{
+                text.style.transitionDelay = `${index * .2}s`
+            });
+        }
+    });
+
+
     const burger = document.querySelector('.burger');
     const header = document.querySelector('.header');
 
