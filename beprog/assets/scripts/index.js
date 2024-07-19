41e7068dfae81d0aa6c10b6202df30cfc901ab4a
kreotib <kreotib@gmail.com>
2024-07-19 18:48:21 +0300

    1234

--- beprog/assets/scripts/index.js
+++ beprog/assets/scripts/index.js
@@ -1,4 +1,43 @@
+class Animations {
+    constructor() {
+        this.animationWrapperArray = document.querySelectorAll('.animation-wrapper');
+    }
+
+    toggleAnimation(animationWrapperElement) {
+        const animationItemArray = animationWrapperElement.querySelectorAll('.animation-item');
+        animationItemArray.forEach((animationItemElement, animationItemIndex) => {
+            animationItemElement.style.animationDelay = `${animationItemIndex * 0.2}s`;
+            animationItemElement.classList.add('animation-item--animated');
+        });
+    }
+
+    handleIntersection(entries) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                this.toggleAnimation(entry.target);
+            }
+        });
+    }
+
+    init() {
+        if (!this.animationWrapperArray.length) return;
+
+        const observer = new IntersectionObserver(this.handleIntersection.bind(this), {
+            // rootMargin:''
+
+        });
+        this.animationWrapperArray.forEach(animationWrapper => {
+            observer.observe(animationWrapper);
+        });
+    }
+}
+
+
 document.addEventListener('DOMContentLoaded', () => {
+    //animation init
+    const animation = new Animations();
+    animation.init();
+
     const videoArray = document.querySelectorAll('.video');
 
     if (videoArray.length) {
