d2dcf6bed6adfda246587d9274567b61857d8371
kreotib <kreotib@gmail.com>
2024-08-29 09:04:12 +0300

    1234

--- invest/assets/scripts/index.js
+++ invest/assets/scripts/index.js
@@ -1,3 +1,38 @@
+class Animations {
+    constructor() {
+        this.animationWrapperArray = document.querySelectorAll('.animation-wrapper');
+    }
+
+    toggleAnimation(animationWrapperElement) {
+        const animationItemArray = animationWrapperElement.querySelectorAll('.animation-item');
+        animationItemArray.forEach((animationItemElement, animationItemIndex) => {
+            animationItemElement.style.setProperty(`--animation-delay`, `${animationItemIndex * 0.2}s`);
+            animationItemElement.classList.add('animated');
+        });
+    }
+
+
+    handleIntersection(entries) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                this.toggleAnimation(entry.target);
+            }
+        });
+    }
+
+    init() {
+        if (!this.animationWrapperArray.length) return;
+
+
+        const observer = new IntersectionObserver(this.handleIntersection.bind(this), {
+            threshold: 0.1
+        });
+        this.animationWrapperArray.forEach(animationWrapper => {
+            observer.observe(animationWrapper);
+        });
+    }
+}
+
 const showPopup = popupId => {
     const popup = document.querySelector(popupId);
     if (!popup) return
@@ -36,6 +71,9 @@ const checkTargetOrKey = event => {
 
 
 document.addEventListener('DOMContentLoaded', () => {
+    const animation = new Animations();
+    animation.init();
+    
     const languageArray = document.querySelectorAll('.language');
 
     console.log('1243');
