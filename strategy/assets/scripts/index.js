54ab24d0814ee55446183b1dd2665bcc8b9a0a13
kreotib <kreotib@gmail.com>
2024-12-11 15:29:02 +0300

    Add files via upload

--- strategy/assets/scripts/index.js
+++ strategy/assets/scripts/index.js
@@ -23,9 +23,8 @@ class Animations {
     init() {
         if (!this.animationWrapperArray.length) return;
 
-        const observer = new IntersectionObserver(this.handleIntersection.bind(this),{
+        const observer = new IntersectionObserver(this.handleIntersection.bind(this), {
             // rootMargin:''
-
         });
         this.animationWrapperArray.forEach(animationWrapper => {
             observer.observe(animationWrapper);
@@ -34,7 +33,7 @@ class Animations {
 }
 
 
-document.addEventListener('DOMContentLoaded',()=>{
+document.addEventListener('DOMContentLoaded', () => {
     //animation init
     const animation = new Animations();
     animation.init();
@@ -42,8 +41,8 @@ document.addEventListener('DOMContentLoaded',()=>{
     const burger = document.querySelector('.burger'),
         menu = document.querySelector('.menu');
 
-    if(burger && menu){
-        burger.addEventListener('click',(e)=>{
+    if (burger && menu) {
+        burger.addEventListener('click', (e) => {
             e.preventDefault();
 
             burger.classList.toggle('burger--active');
@@ -53,10 +52,10 @@ document.addEventListener('DOMContentLoaded',()=>{
 
         const anchorLinks = document.querySelectorAll('a[href^="#"]');
 
-        if(anchorLinks.length){
+        if (anchorLinks.length) {
             anchorLinks.forEach(link => {
-                if(link.hash.length > 0){
-                    link.addEventListener('click',()=>{
+                if (link.hash.length > 0) {
+                    link.addEventListener('click', () => {
                         burger.classList.remove('burger--active');
                         menu.classList.remove('menu--active');
                         document.body.classList.remove('no-scroll');
@@ -67,31 +66,78 @@ document.addEventListener('DOMContentLoaded',()=>{
     }
 
 
+    const principlesCardList = document.querySelectorAll('.principles-card'),
+        principlesCardGrid = document.querySelector('.principles-grid');
+
+    if (principlesCardList.length) {
+        principlesCardList[principlesCardList.length - 1].classList.add('principles-card--big');
+        principlesCardList[principlesCardList.length - 2].classList.add('principles-card--middle');
+
+        const targetElements = document.querySelectorAll('.principles');
+
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    principlesCardList.forEach((card,cardIndex)=>{
+                        setTimeout(()=>{
+                            card.classList.remove('principles-card--animated');
+                        }, cardIndex * 1000)
+                    })
+                }
+            });
+        }, {
+            threshold: 0.1
+        });
+
+        targetElements.forEach(element => {
+            observer.observe(element);
+        });
+
+        principlesCardList.forEach((card, cardIndex) => {
+            card.classList.add('principles-card--animated')
+
+            card.addEventListener('click', (e) => {
+                e.preventDefault();
+
+                if (card.classList.contains('principles-card--big')) return;
+
+                principlesCardList.forEach(otherCard => {
+                    otherCard.classList.remove('principles-card--active');
+                    otherCard.classList.remove('principles-card--middle');
+                    if (otherCard.classList.contains('principles-card--big')) otherCard.classList.add('principles-card--middle');
+
+                    otherCard.classList.remove('principles-card--big');
+                });
+
+                card.classList.add('principles-card--big');
+            });
+        });
+    }
 
 
     const teamSliderElement = document.querySelector('.team-slider-element');
 
-    if(teamSliderElement){
+    if (teamSliderElement) {
         const teamSlider = new Swiper('.team-slider-element', {
-            slidesPerView:'auto',
-            spaceBetween:10,
+            slidesPerView: 'auto',
+            spaceBetween: 10,
             grid: {
-                fill:'column',
-                rows:1,
-                slidesOffsetAfter:50,
+                fill: 'column',
+                rows: 1,
+                slidesOffsetAfter: 50,
             },
-            breakpoints:{
-                1239:{
-                    slidesPerView:4,
+            breakpoints: {
+                1239: {
+                    slidesPerView: 4,
                     grid: {
-                        fill:'row',
+                        fill: 'row',
                         rows: 2,
                     },
                 },
-                767:{
-                    slidesPerView:2,
+                767: {
+                    slidesPerView: 2,
                     grid: {
-                        fill:'row',
+                        fill: 'row',
                         rows: 4,
                     },
                 }
